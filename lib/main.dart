83a4eea18a0cc33f620c3957e2420434329c3950
PharanBrush <79798205+pharanbrush@users.noreply.github.com>
2023-10-02 00:29:39 +0800

    Focus main window when loaded

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ void main() async {
 
   windowManager.waitUntilReadyToShow(windowOptions, () async {
     await windowManager.show();
-    //await windowManager.focus();
+    await windowManager.focus();
   });
 
   runApp(MyApp(
