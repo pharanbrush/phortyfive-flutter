ee8c3b6774f36fe5779951d06d35c84d1e8b2f9f
PharanBrush <79798205+pharanbrush@users.noreply.github.com>
2023-10-05 20:01:54 +0800

    Add transparent modal underlay option.

--- lib/widgets/modal_underlay.dart
+++ lib/widgets/modal_underlay.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 class ModalUnderlay extends StatelessWidget {
-  const ModalUnderlay({super.key, this.onTapDown});
+  const ModalUnderlay({super.key, this.onTapDown, this.isTransparent = false});
 
   final Function()? onTapDown;
+  final bool isTransparent;
+
+  static const fadeStyle = BoxDecoration(color: Colors.white60);
+  static const transparentStyle = BoxDecoration(color: Colors.transparent);
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +15,7 @@ class ModalUnderlay extends StatelessWidget {
       child: GestureDetector(
         onTapDown: (details) => onTapDown!(),
         child:
-            Container(decoration: const BoxDecoration(color: Colors.white60)),
+            Container(decoration: isTransparent ? transparentStyle : fadeStyle),
       ),
     );
   }
