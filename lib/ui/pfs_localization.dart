33bce40cb83417c65dd046382282a0705c7dce42
PharanBrush <79798205+pharanbrush@users.noreply.github.com>
2023-10-12 01:30:07 +0800

    PfsLocalization to help label consistency.

---
+++ lib/ui/pfs_localization.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PfsLocalization {
+  static const String alwaysOnTop = 'Keep window on top';
+  static const String help = 'Help...';
+
+  static String tooltipShortcut(SingleActivator shortcut) {
+    var keyString = shortcut.trigger.keyLabel;
+    return '${shortcut.control ? 'Ctrl+' : ''}${shortcut.shift ? 'Shift+' : ''}$keyString';
+  }
+
+  static String buttonTooltip({
+    required String commandName,
+    required SingleActivator shortcut,
+  }) {
+    return '$commandName (${tooltipShortcut(shortcut)})';
+  }
+}
