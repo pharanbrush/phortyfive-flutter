831da15475611c13b7c7fcc69ac13dabf10450ca
PharanBrush <79798205+pharanbrush@users.noreply.github.com>
2024-08-13 22:38:07 +0800

    Add base image_annotation code.

---
+++ lib/widgets/image_annotation/image_annotation.dart
@@ -0,0 +1,7 @@
+library image_annotation;
+
+// Based on image_annotation by Mikita Drazdou
+
+export 'src/annotation_widget.dart';
+export 'src/annotation_painter.dart';
+export 'src/text_annotation.dart';
