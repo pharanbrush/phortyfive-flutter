75565dc5799e268ad9c28d7b7664b1248476b7c0
PharanBrush <79798205+pharanbrush@users.noreply.github.com>
2025-11-16 13:32:18 +0800

    Rename window on top button file.

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:pfs2/core/circulator.dart';
 import 'package:pfs2/models/pfs_model.dart';
 import 'package:pfs2/models/phtimer_model.dart';
 import 'package:pfs2/main_screen/main_screen.dart';
-import 'package:pfs2/phlutter/bitsdojo_phwindow.dart';
+import 'package:pfs2/phlutter/keep_window_on_top_button.dart';
 import 'package:pfs2/phlutter/escape_route.dart';
 import 'package:pfs2/ui/themes/pfs_theme.dart';
 import 'package:pfs2/ui/themes/window_button_colors.dart';
