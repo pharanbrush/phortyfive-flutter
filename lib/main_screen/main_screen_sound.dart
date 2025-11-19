5eabf835bb1712871c0024a30cd64fa3849f47ad
PharanBrush <79798205+pharanbrush@users.noreply.github.com>
2025-11-19 19:53:32 +0800

    Update audioplayers API usage comment as a reminder.

--- lib/main_screen/main_screen_sound.dart
+++ lib/main_screen/main_screen_sound.dart
@@ -23,7 +23,10 @@ mixin MainScreenSound {
   // }
 
   void _playSound() async {
-    _player.play(_clickSound);
+    // Probably don't await or try to do anything with the Future this returns
+    // To prevent timing problems and crashing because of platform thread compatibility.
+    // This seems to be a unpatched issue even in audioplayers 6.2
+    _player.play(_clickSound); 
     //await _player.setPlaybackRate(_getRandomPitch());
   }
 }
