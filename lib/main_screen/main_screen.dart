dc50a42c1e8a5f3660b9dca65bcf082a13c70523
PharanBrush <79798205+pharanbrush@users.noreply.github.com>
2025-11-21 00:57:48 +0800

    Add dummy undo handler.

--- lib/main_screen/main_screen.dart
+++ lib/main_screen/main_screen.dart
@@ -119,6 +119,7 @@ class _MainScreenState extends State<MainScreen>
     (ZoomOutIntent, (_) => imagePhviewer.incrementZoomLevel(-1)),
     (ZoomResetIntent, (_) => imagePhviewer.resetTransform()),
     (PasteIntent, (_) => tryPaste()),
+    (UndoIntent, (_) => tryUndo()),
   ];
 
   final Map<Type, Action<Intent>> firstScreenShortcutActions = {};
@@ -731,6 +732,10 @@ class _MainScreenState extends State<MainScreen>
         return imageBrowseBottomBar();
       },
     );
+  }
+
+  void tryUndo() {
+    debugPrint("Undo!");
   }
 }
 
