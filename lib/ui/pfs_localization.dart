6fcc0f15b4e984aa138efdbad3bff3774a85c520
PharanBrush <79798205+pharanbrush@users.noreply.github.com>
2026-09-04 02:43:19 +0800

    Make "Copy image" case consistent with other menu items.

--- lib/ui/pfs_localization.dart
+++ lib/ui/pfs_localization.dart
@@ -15,7 +15,7 @@ class PfsLocalization {
   static const String shortcutHelp = "Shortcut help...";
   static const String revealInExplorer = "Reveal file in Explorer";
   static const String copyFilePath = "Copy file path";
-  static const String copyImageToClipboard = "Copy Image";
+  static const String copyImageToClipboard = "Copy image";
   static const String copyFileName = "Copy filename";
 
   static const String timerPlaying = "Timer running";
